7c5f4654952d0181bc82ccbb678fb1104938e7d6
Marius Alban <malban298@gmail.com>
2024-02-13 15:30:10 -0300

    event handle function

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleFocus(event) {
+        console.log("Good!")
+      }
+      function handleBlur(event) {
+        console.log("Hey! Eyes on me!")
+      }
+
+    return(
+<div>
+<button onBlur={handleBlur} onFocus={handleFocus}>"Eyes on me"</button>
+</div> 
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
-// Code Keypad Component Here
+import React from 'react';
+
 
 function Keypad (){
+
     return (
-        <div></div>
+        <div>
+            <form>
+            <input type="password" onChange={(event)=>  console.log("Entering password...")} />
+            </form>
+        </div>
     )
 }
 
